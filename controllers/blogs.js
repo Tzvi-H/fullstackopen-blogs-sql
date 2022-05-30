5c35ef1a10138c8e0fea51cf95668cdac238a889
Tzvi-H <16guitar@gmail.com>
2022-05-30 10:37:55 -0600

    completed Exercise 13.10.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,27 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
 
-const { Blog } = require("../models");
+const { Blog, User } = require("../models");
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id);
   next();
+};
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+      next();
+    } catch (e) {
+      console.error(e);
+      res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    res.status(401).json({ error: "token missing" });
+  }
 };
 
 router.get("/", async (req, res) => {
@@ -12,8 +29,9 @@ router.get("/", async (req, res) => {
   res.json(blogs);
 });
 
-router.post("/", async (req, res) => {
-  const blog = await Blog.create(req.body);
+router.post("/", tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const blog = await Blog.create({ ...req.body, userId: user.id });
   res.json(blog);
 });
 
